aa261a3cc5571ccfb001f841d883467dd42bcbc1
NikhilOli <nikhiloli107@gmail.com>
2024-03-22 23:56:27 +0545

    added functionality to keyword search buttons

--- pexelsAPI/src/components/Navbar.jsx
+++ pexelsAPI/src/components/Navbar.jsx
@@ -29,14 +29,16 @@ const Navbar = ({ setSearch }) => {
                 </div>
             </nav>
             <div className='flex justify-center items-center gap-10 my-2'>
-                <button className={`bg-red-400 text-white py-2 px-4 rounded`}> Nature </button>
-                <button className={`bg-cyan-400 text-white py-2 px-4 rounded`}> Nature </button>
-                <button className={`bg-emerald-400 text-white py-2 px-4 rounded`}> Nature </button>
-                <button className={`bg-rose-400 text-white py-2 px-4 rounded`}> Nature </button>
-                <button className={`bg-indigo-400 text-white py-2 px-4 rounded`}> Nature </button>
-                <button className={`bg-purple-400 text-white py-2 px-4 rounded`}> Nature </button>
-                <button className={`bg-violet-400 text-white py-2 px-4 rounded`}> Nature </button>
-                <button className={`bg-teal-400 text-white py-2 px-4 rounded`}> Nature </button>
+                <button className={`bg-red-400 text-white py-2 px-4 rounded`} onClick={() => setSearch("Nature")}> Nature </button>
+                <button className={`bg-cyan-400 text-white py-2 px-4 rounded`} onClick={() => setSearch("Travel")}> Travel </button>
+                <button className={`bg-emerald-400 text-white py-2 px-4 rounded`} onClick={() => setSearch("Animals")}> Animals </button>
+                <button className={`bg-orange-400 text-white py-2 px-4 rounded`} onClick={() => setSearch("Music")}> Music </button>
+                <button className={`bg-zinc-400 text-white py-2 px-4 rounded`} onClick={() => setSearch("Sports")}> Sports </button>
+                <button className={`bg-rose-400 text-white py-2 px-4 rounded`} onClick={() => setSearch("Food")}> Food </button>
+                <button className={`bg-indigo-400 text-white py-2 px-4 rounded`} onClick={() => setSearch("Technology")}> Technology </button>
+                <button className={`bg-purple-400 text-white py-2 px-4 rounded`} onClick={() => setSearch("Art")}> Art </button>
+                <button className={`bg-violet-400 text-white py-2 px-4 rounded`} onClick={() => setSearch("Fashion")}> Fashion </button>
+                <button className={`bg-teal-400 text-white py-2 px-4 rounded`} onClick={() => setSearch("Architecture")}> Architecture </button>
             </div>
         </>
     );
