b287cdbbbbf106ab8bc462cd1cb93102e1c1d395
NikhilOli <nikhiloli107@gmail.com>
2024-03-29 00:53:58 +0545

    initial setup complete with yarn

---
+++ react-framer-portfolio/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-red-500 text-4xl'>This is my first yarn package manager react app</div>
+  )
+}
+
+export default App
